6859376ac7c5b96183940a6527011d7d86daca89
WayneLi1063 <weileli@uw.edu>
2018-11-07 14:21:16 -0800

    gives it a color

--- elev.R
+++ elev.R
@@ -1,6 +1,7 @@
 library(dplyr)
-elev_data <- read.delim("uw-elevations.csv.bz2")
+elev_data <- read.delim("uw-elevations.csv.bz2") %>% mutate(elevation = ifelse(elevation < 5.5, NA, elevation))
+
 library(ggplot2)
-ggplot(elev_data, aes(lon, lat, col=elevation)) + geom_point() + coord_quickmap()
+ggplot(elev_data, aes(lon, lat, fill=elevation)) + geom_tile() + coord_quickmap() + scale_fill_gradientn(colors=terrain.colors(10), na.value="steelblue3")
 dev.copy(png, "uw-elevation.png")
 dev.off()
